62333cc5010d269bcbb73e6312d424441fd79585
Lesego Kgatle <kgatlelesego2@gmail.com>
2026-04-26 19:49:21 +0200

    added more weather data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-
   let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElelment = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function formatDate(date) {
+  let day = date.getDay();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+}
+let day = days[date.getDay()];
+
+if (minutes <10 ){minutes = `0${minutes}`};
+
+return `${day} ${hours}:${minutes}`
 
 function searchCity(city) {
   let apiKey = "e3a4ea3eobaf1tb802ba5309a36047f0";
