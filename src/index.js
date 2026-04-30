dd8e29574654afd23f2f459156f74aa9615f9acf
Lesego Kgatle <kgatlelesego2@gmail.com>
2026-04-30 13:14:34 +0200

    Added the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -52,7 +52,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+                    <div class="weather-forecast-date">
+                      ${day}
+                    </div>
+                    <div class="weather-forecast-icon">
+                        ☀️
+                    </div>
+                    <div class="weather-forecast-temps">
+                        <div class="temp1">15°C </div>
+                        <div class="temp1">9°C</div>
+                    </div>
+                </div>
+                `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
